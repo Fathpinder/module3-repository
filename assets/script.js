7d65ebf44190cba9c21f0a5d3f248629558be71e
markg <mark.grangaard@gmail.com>
2022-05-23 15:21:20 -0500

    completed random password generator

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Code to prompt user for pw length and character options
 function generatePassword() {
   console.log("you cliked the button");
   var lengthPrompt = function() {
@@ -15,64 +16,76 @@ function generatePassword() {
     
     if (pwLength >= 8 || pwLength <= 128) {
       window.alert("your password will be " + pwLength + " characters");
+      
       return pwLength;
     }
     return false;
   
   }
   var pwLength = lengthPrompt();
+ 
   console.log(pwLength);
-  
-  var charSet = function() {
-    var upperSelect = window.confirm("Include uppercase letters?");
-    if (upperSelect) {
-      console.log("include uppercase");
-      
-    } else {
-      console.log('no uppercase');
-    }
-    var lowerSelect = window.confirm("Include lowercase letters?");
-    if (lowerSelect) {
-      console.log("include lowercase");
-      
-    } else {
-      console.log("no lowercase");
-    }
-    var numSelect = window.confirm("Include numbers?");
-    if (numSelect) {
-      console.log("include numbers");
-      
-    } else {
-      console.log("no numbers");
-    }
-    var specSelect = window.confirm("Include special characters?");
-    if (specSelect) {
-      console.log("include special"); 
-    }
-    else {
-      console.log("no special");
-    }
-    if (!upperSelect && !lowerSelect && !numSelect && !specSelect) {
-      window.alert("your password must contain characters. try again.");
-      return charSet();
-    } else if (upperSelect && !lowerSelect && !numSelect && !specSelect) {
-
-    }
-  }
-  charSet();
   var passwordOptions = {
-    LOWERCASE: "abcdefghijklmnopqrstuvwxyz",
-    UPPERCASE: "ABCDEFGHIJKLMOPQRSTUVWXYZ",
-    NUMERIC: "1234567890",
-    SPECIAL: "!@#$%^&*_+-=",
     length: pwLength,
   }
   console.log(passwordOptions);
   
-  var randProp = function(passwordOptions) {
-    var LOWERCASE = passwordOptions.LOWERCASE(passwordOptions);
-    return passwordOptions[LOWERCASE[ pwLength * Math.random() << 0]];
+  // function to determine what characters to include in pw
+  var charSet = function() {
+    var upperSelect = window.confirm("Include uppercase letters?");
+    if (upperSelect) {
+      console.log("include uppercase");
+      passwordOptions.UPPERCASE = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+    } else {
+      console.log('no uppercase');
+      passwordOptions.UPPERCASE = "";
+    }
+    var lowerSelect = window.confirm("Include lowercase letters?");
+    if (lowerSelect) {
+      console.log("include lowercase");
+      passwordOptions.LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    } else {
+      console.log("no lowercase");
+      passwordOptions.LOWERCASE = "";
+    }
+    var numSelect = window.confirm("Include numbers?");
+    if (numSelect) {
+      console.log("include numbers");
+      passwordOptions.NUMERIC = 1234567890;
+    } else {
+      console.log("no numbers");
+      passwordOptions.NUMERIC = "";
+    }
+    var specSelect = window.confirm("Include special characters?");
+    if (specSelect) {
+      console.log("include special");
+      passwordOptions.SPECIAL = "!@#$%^&*_+-=";
+        }
+    else {
+      console.log("no special");
+      passwordOptions.SPECIAL = "";
+    }
+    if (!upperSelect && !lowerSelect && !numSelect && !specSelect) {
+      window.alert("your password must contain characters. try again.");
+      return charSet();
+    } 
   }
+  charSet();
+  console.log(passwordOptions);
+  // create string of characters to include in password
+  var charOptions = {
+    list: passwordOptions.UPPERCASE + passwordOptions.LOWERCASE + passwordOptions.NUMERIC + passwordOptions.SPECIAL,
+  }
+  console.log("randomizing " + charOptions.list.length + " options");
+  console.log(charOptions);
+// generate random password
+  var password = "";
+  var options = charOptions.list;
+  for (i = 0; i < pwLength; i++) {
+    password += options.charAt(Math.floor(Math.random() * options.length));
+  }
+  return password;
+  console.log(password);
 };
 
 // Write password to the #password input
